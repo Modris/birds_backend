b3d232a9a627ec08b04703f8f9febf99b03d4212
modris <modrislauzis@gmail.com>
2023-12-12 10:56:13 +0200

    Add dev properties.

---
+++ src/main/java/com/modris/config/ProjectConfig.java
@@ -0,0 +1,9 @@
+package com.modris.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "birds")
+public class ProjectConfig {
+
+	
+}
